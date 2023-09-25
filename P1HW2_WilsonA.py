ccb99f5c063bb8cb7c3ec8c2b3791d92fe3419fd
wilsona5126 <145864434+wilsona5126@users.noreply.github.com>
2023-09-25 14:17:36 -0400

    Add files via upload

---
+++ P1HW2_WilsonA.py
@@ -0,0 +1,12 @@
+# Calculating travel Expenses
+# 9/23/2023
+# CTI 110 P1HW2 - Travel Expense
+# WilsonA
+
+your_budget = float(input("What's your budget?"))
+input("Where are you going?")
+cost = float(input('How much do you think you will spend on gas?'))
+accom = float(input('Are you driving straight through? Hotel?'))
+food = float(input('How much do you need for food?'))   
+print(float(your_budget - cost - accom- food))
+
